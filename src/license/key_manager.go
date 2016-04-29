76500924cce178c1d6c99d0d50e277b875b6b2fc
Benjamin Pannell <admin@sierrasoftworks.com>
2016-04-29 14:30:46 +0200

    feat: Add key manager implementation and tests

---
+++ src/license/key_manager.go
@@ -0,0 +1,220 @@
+package license
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// PrivateKeyType is used to armour the machine's private key when
+// PEM encoded and encrypted.
+const PrivateKeyType = "LITHIUM PRIVATE KEY"
+
+// PrivateKeyName is the name of the private key file. It may be
+// changed if you wish to enable multiple side-by-side installations
+// with different keys. This is usually not necessary.
+var PrivateKeyName = "machine"
+
+// PublicKeyType is used to armour the machine's public key when
+// PEM encoded.
+const PublicKeyType = "LITHIUM PUBLIC KEY"
+
+// PublicKeyName is the name of your public key file. It may be
+// changed if you wish to enable multiple side-by-side installations
+// with different keys. This is usually not necessary.
+var PublicKeyName = "machine.pub"
+
+// KeyManager provides a set of tools for accessing your machine's
+// local key. This is tied to your machine and allows you to decrypt
+// your license packs.
+type KeyManager struct {
+	MachineCode []byte
+	Path        string
+}
+
+// NewKeyManager returns a new KeyManager for your local machine using
+// the given machine code to encrypt and decrypt local machine keys.
+func NewKeyManager(machineCode []byte) *KeyManager {
+	homeDir := os.ExpandEnv("$HOME")
+	licenseFolder := filepath.Join(homeDir, ".lithium")
+
+	return &KeyManager{
+		MachineCode: machineCode,
+		Path:        licenseFolder,
+	}
+}
+
+// GetPublicKey retrieves the public key for your local machine. This
+// is used by upstream servers to identify and encrypt keys for your
+// machine.
+func (m *KeyManager) GetPublicKey() (*ecdsa.PublicKey, error) {
+	err := m.ensureKeypair()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := m.readKeyFile(PublicKeyName)
+	if err != nil {
+		return nil, err
+	}
+
+	pubBlock, _ := pem.Decode(data)
+	if pubBlock == nil {
+		return nil, errors.New("machine key was not a valid PEM block")
+	}
+
+	if pubBlock.Type != PublicKeyType {
+		return nil, errors.New("machine key was not of the correct type")
+	}
+
+	pub, err := x509.ParsePKIXPublicKey(pubBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	switch pub.(type) {
+	case *ecdsa.PublicKey:
+		return pub.(*ecdsa.PublicKey), nil
+	default:
+		return nil, errors.New("only ECDSA public keys supported")
+	}
+}
+
+// GetPrivateKey retrieves the private key for your local machine. This
+// is used to decrypt license packs and sign child license files for
+// later verification.
+func (m *KeyManager) GetPrivateKey() (*ecdsa.PrivateKey, error) {
+	err := m.ensureKeypair()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := m.readKeyFile(PrivateKeyName)
+	if err != nil {
+		return nil, err
+	}
+
+	privBlock, _ := pem.Decode(data)
+	if privBlock == nil {
+		return nil, errors.New("machine key was not a valid PEM block")
+	}
+
+	if privBlock.Type != PrivateKeyType {
+		return nil, errors.New("machine key was not of the correct type")
+	}
+
+	if !x509.IsEncryptedPEMBlock(privBlock) {
+		return nil, errors.New("expected machine key to be encrypted")
+	}
+
+	privData, err := x509.DecryptPEMBlock(privBlock, m.MachineCode)
+	if err != nil {
+		return nil, err
+	}
+
+	priv, err := x509.ParseECPrivateKey(privData)
+	if err != nil {
+		return nil, err
+	}
+
+	return priv, nil
+}
+
+// ResetKeypair will generate a new keypair for this machine, replacing
+// the existing keypair and invalidating any licenses which were created
+// for it.
+func (m *KeyManager) ResetKeypair() error {
+	return m.createKeypair()
+}
+
+func (m *KeyManager) ensureKeypair() error {
+	if !m.keyFileExists(PrivateKeyName) || !m.keyFileExists(PublicKeyName) {
+		return m.createKeypair()
+	}
+
+	return nil
+}
+
+func (m *KeyManager) keyFileExists(file string) bool {
+	f, err := os.Open(m.getLicenseFilePath(file))
+	if err == nil {
+		defer f.Close()
+	}
+
+	if err != nil && os.IsNotExist(err) {
+		return false
+	} else if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func (m *KeyManager) createKeypair() error {
+	priv, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	derPrivateKey, err := x509.MarshalECPrivateKey(priv)
+	if err != nil {
+		return err
+	}
+
+	encryptedPrivateKey, err := x509.EncryptPEMBlock(
+		rand.Reader,
+		PrivateKeyType,
+		derPrivateKey,
+		m.MachineCode,
+		x509.PEMCipherAES256,
+	)
+
+	err = m.writeKeyFile(PrivateKeyName, pem.EncodeToMemory(encryptedPrivateKey))
+	if err != nil {
+		return err
+	}
+
+	pub := priv.Public()
+	pubKeyData, err := x509.MarshalPKIXPublicKey(pub)
+	if err != nil {
+		return err
+	}
+
+	pubKeyBytes := pem.EncodeToMemory(&pem.Block{
+		Type:  PublicKeyType,
+		Bytes: pubKeyData,
+	})
+
+	return m.writeKeyFile(PublicKeyName, pubKeyBytes)
+}
+
+func (m *KeyManager) writeKeyFile(file string, data []byte) error {
+	f, err := os.Create(m.getLicenseFilePath(file))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(data)
+	return err
+}
+
+func (m *KeyManager) readKeyFile(file string) ([]byte, error) {
+	f, err := os.Open(m.getLicenseFilePath(file))
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return ioutil.ReadAll(f)
+}
+
+func (m *KeyManager) getLicenseFilePath(file string) string {
+	return filepath.Join(m.Path, file)
+}
---
+++ src/license/key_manager_test.go
@@ -0,0 +1,221 @@
+package license
+
+import (
+	"encoding/pem"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestNewKeyManager(t *testing.T) {
+	machineCode := []byte("test")
+
+	m := NewKeyManager(machineCode)
+	if m == nil {
+		t.Fatal("expected NewKeyManager to return a key manager")
+	}
+
+	if !reflect.DeepEqual(m.MachineCode, machineCode) {
+		t.Errorf("expected machine code to be %#v but got %#v instead.", machineCode, m.MachineCode)
+	}
+
+	if m.Path == "" {
+		t.Errorf("expected path to be a defined value")
+	}
+}
+
+func TestGetPublicKey(t *testing.T) {
+	machineCode := []byte("test")
+	tempDir := os.TempDir()
+	testPath, err := ioutil.TempDir(tempDir, "lithium")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testPath)
+
+	m := NewKeyManager(machineCode)
+	m.Path = testPath
+
+	key, err := m.GetPublicKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if key == nil {
+		t.Fatalf("expected public key to be defined")
+	}
+
+	if key.X == nil {
+		t.Errorf("expected key.X to be defined")
+	}
+
+	if key.Y == nil {
+		t.Errorf("expected key.Y to be defined")
+	}
+
+	f, err := os.Open(filepath.Join(testPath, PublicKeyName))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	block, _ := pem.Decode(data)
+	if block == nil {
+		t.Fatal("expected key file to be a valid PEM block")
+	}
+
+	if block.Type != PublicKeyType {
+		t.Errorf("expected PEM block to be of type '%s'", PublicKeyType)
+	}
+}
+
+func TestPublicKeyPersistence(t *testing.T) {
+	machineCode := []byte("test")
+	tempDir := os.TempDir()
+	testPath, err := ioutil.TempDir(tempDir, "lithium")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testPath)
+
+	m := NewKeyManager(machineCode)
+	m.Path = testPath
+
+	key, err := m.GetPublicKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	key2, err := m.GetPublicKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(key, key2) {
+		t.Error("expected to receive the same keys")
+	}
+}
+
+func TestGetPrivateKey(t *testing.T) {
+	machineCode := []byte("test")
+	tempDir := os.TempDir()
+	testPath, err := ioutil.TempDir(tempDir, "lithium")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testPath)
+
+	m := NewKeyManager(machineCode)
+	m.Path = testPath
+
+	key, err := m.GetPrivateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if key == nil {
+		t.Fatalf("expected private key to be defined")
+	}
+
+	if key.X == nil {
+		t.Errorf("expected key.X to be defined")
+	}
+
+	if key.Y == nil {
+		t.Errorf("expected key.Y to be defined")
+	}
+
+	if key.D == nil {
+		t.Errorf("expected key.D to be defined")
+	}
+
+	f, err := os.Open(filepath.Join(testPath, PrivateKeyName))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	block, _ := pem.Decode(data)
+	if block == nil {
+		t.Fatal("expected key file to be a valid PEM block")
+	}
+
+	if block.Type != PrivateKeyType {
+		t.Errorf("expected PEM block to be of type '%s'", PrivateKeyType)
+	}
+}
+
+func TestPrivateKeyPersistence(t *testing.T) {
+	machineCode := []byte("test")
+	tempDir := os.TempDir()
+	testPath, err := ioutil.TempDir(tempDir, "lithium")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testPath)
+
+	m := NewKeyManager(machineCode)
+	m.Path = testPath
+
+	key, err := m.GetPrivateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	key2, err := m.GetPrivateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(key, key2) {
+		t.Error("expected to receive the same keys")
+	}
+}
+
+func TestResetKeys(t *testing.T) {
+	machineCode := []byte("test")
+	tempDir := os.TempDir()
+	testPath, err := ioutil.TempDir(tempDir, "lithium")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testPath)
+
+	m := NewKeyManager(machineCode)
+	m.Path = testPath
+
+	key, err := m.GetPrivateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if key == nil {
+		t.Fatalf("expected private key to be defined")
+	}
+
+	err = m.ResetKeypair()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	newKey, err := m.GetPrivateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reflect.DeepEqual(key, newKey) {
+		t.Error("expected a new public key to have been generated")
+	}
+}
